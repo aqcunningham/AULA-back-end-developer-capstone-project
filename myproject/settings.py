025caf3055084f6c51ec8ffbca0aed3c266e2f6b
A Cunningham <21978358+aqcunningham@users.noreply.github.com>
2026-08-31 15:46:16 -0400

    more wrappin

--- myproject/settings.py
+++ myproject/settings.py
@@ -105,6 +105,7 @@ DATABASES = {
     }
 }
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
